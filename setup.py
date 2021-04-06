e6af85b1059bd4e24fbe4043ede7d14ce8d8b7c2
goforbroke <go.for.broke1006@gmail.com>
2021-04-06 19:26:53 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='simulation-steps',
+      version='1.0',
+      description='Behave steps to reproduce bugs',
+      author='Sergey Cherkesov',
+      author_email='go.for.broke1006@gmail.com',
+      url='https://github.com/goforbroke1006/simulation-steps',
+      packages=[
+          'features',
+          'features.misc',
+      ],
+      )
